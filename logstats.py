c018f008baacf3b542291544120c0e3a2e1e6265
gg <gorazd@stricek.(none)>
2013-10-13 00:21:26 +0200

    added req/s analysis, licence info, output formating, export of value distribution

--- logstats.py
+++ logstats.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf8 :
+'''
+   Copyright 2013  Gorazd Golob
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+'''
+
+
 
 import time, sys, os, random, getopt
 
@@ -9,9 +28,9 @@ def usage():
     print "logstats.py exports simple statistics of response times given by stdin"
     print "default input stream should include a response times in onlt or in second field separated by coma" 
     print " -h current output"
+    print " -d exports also distribution of values in csv"
     # TODO
     #print " -z host=<host> exports zabbix agent format as <host> value <value> " 
-    #print " -d exports also distribution of values in csv"
     #print " -t include req/s analytics" 
     #print " -d and -t can be combined to export distribution of req/s values"
 
@@ -62,22 +81,32 @@ def read_to_array_file(inf):
         fsock.close()	
     return j
 
-def read_to_array():
+def read_to_dict():
     # obsolete
     j = {}
+    req = {}
+    # TODO: loop below should be done more general regardles the data input 
     for onel in sys.stdin:
         # very specific - cvs with two values in place
         both = str.split(onel,',')
         if len(both)>1:
+            if len(str(both[0]))>10:
+                timez = int(int(both[0])/1000)
+            else:
+                timez = int(both[0])
             value = to_mil(float(both[1]))
         else:
             value = to_mil(float(onel.rstrip()))
+            timez = None
         if value in j.keys():
             j[value] = j[value] + 1
         else:
             j[value] = 1
-    return j
-
+        if timez in req.keys():
+            req[timez] = req[timez] + 1
+        else:
+            req[timez] = 1
+    return {'values':j, 'requests':req}
 
 
 
@@ -102,6 +131,43 @@ def count_percentiles(in_dict):
     perc["avg"] = sum_favg / len(in_dict)
     return perc
 
+def count_reqs(in_dict):
+    i = 0
+    max_reqs = 0 
+    for k, v in in_dict.iteritems():
+        if i == 0:
+            min_reqs = v
+        if v < min_reqs:
+            min_reqs = v
+        if v > max_reqs:
+            max_reqs = v 
+        i += v
+    return {'avg':round(float(i)/len(in_dict),2),'min':min_reqs,'max':max_reqs}
+
+def simple_out(resdict, reqsdict):
+    print "response times"
+    for k in sorted(resdict.iterkeys()):
+        try:
+            if float(k) > 0:
+                outk = 'perc' + str(int (k*100))
+        except:
+            outk = k
+        print outk+','+str(resdict[k])
+    if reqsdict != None:
+        print "requests per second"
+        for k in sorted(reqsdict.iterkeys()):
+            print str(k)+','+str(reqsdict[k])
+    
+def more_out(dicts):
+    print "distribution of values - reponse times"
+    for k in sorted(dicts['values'].iterkeys()):
+        print str(k),',',str(dicts['values'][k])
+    
+    if len(dicts['requests'])>1:
+        print "distribution of values - requests per second"
+        for k in sorted(dicts['requests'].iterkeys()):
+               print str(k)+','+str(dicts['requests'][k])
+
 # do not forget to cound reqs/sec in similar fasgion as req times with deltas!
 
 if __name__ == "__main__":
@@ -111,7 +177,7 @@ if __name__ == "__main__":
 
     #new_dict = read_to_array_file('ts-01.prod.log')
     #new_dict = read_to_array_file('test.log')
-
+    distv = False
     try:
         opts, args = getopt.getopt(sys.argv[1:], "zhdt", ["host="])
     except getopt.GetoptError as err:
@@ -123,18 +189,21 @@ if __name__ == "__main__":
         if o == "-h":
             usage()
             sys.exit(0)
-        elif o == '-z':
-            print 'zabbix mode'
+        elif o == '-d':
+            distv = True
         else:
             usage()
             sys.exit(2)
 
-    new_dict = read_to_array()
-
-    print new_dict
-    print "\n"
-    res = count_percentiles(new_dict)
-    print res
+    new_dict = read_to_dict()
+    res = count_percentiles(new_dict['values'])
+    if len(new_dict['requests'])>1:
+        reqs = count_reqs(new_dict['requests'])
+    else:
+        reqs = None
+    simple_out(res,reqs)
+    if distv:
+        more_out(new_dict)
     
     
 
